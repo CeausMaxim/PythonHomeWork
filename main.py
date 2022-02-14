81061ec97fba61774b1409dc9aa9e0b64105a8ac
CeausMaxim <chaushm@mail.ru>
2022-02-14 10:13:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#Scrieti un program care afiseaza toate elementele din un inventar si calculeaza suma lor.
+inventar = {"scaune":44, "mese":33}
+res = list(inventar.values())
+print(sum(res))
+print(res)
